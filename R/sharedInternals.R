d1fa3296d2d97a1e83888f67998e99d0b3dc0f66
Nicolas Descostes <nicolas.descostes@gmail.com>
2019-07-02 15:30:37 +0200

    add .choosePalette

--- R/sharedInternals.R
+++ R/sharedInternals.R
@@ -28,3 +28,49 @@
 					"2D-tSNE plots."))
 	return(tSNECoordinates)
 }
+
+
+
+#' Depending if the number of clusters is more than 12 or not, one of two built-in palettes will be applied.
+#' If you give your vector of colors, the function will not change them.
+#' If the number of clusters is more than 26, it will copy colors to get the needed length of the palette.
+#'
+#' @param colorPalette Either "default" or a vector of colors, for example c("yellow", "#CC79A7"). 
+#' @param clustersNumber number of clusters in the output palette.
+#'
+#' @return Color palette with the number of colors equal to the clusterNumber parameter.
+#' @export
+.choosePalette <- function(colorPalette, clustersNumber){
+	
+	colorPalette26 <- c( "yellow", "darkgoldenrod1", "coral1", "deeppink",
+			"indianred", "coral4", "darkblue", "darkmagenta",
+			"darkcyan", "mediumorchid", "plum2", "gray73",
+			"cadetblue3", "khaki",
+			"darkolivegreen1", "lightgreen", "limegreen",
+			"darkolivegreen4", "green", "#CC79A7", "violetred3",
+			"brown3", "darkslategray1", "gray51", "slateblue2",
+			"blue")
+	
+	pickDefaultPalette <- function(clustersNumber, colorPalette26){
+		if(clustersNumber < 13) return(c("#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C",
+							"#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00",
+							"#CAB2D6", "#6A3D9A", "#FFFF99",
+							"#B15928")[1:clustersNumber])
+		return(rep(colorPalette26,
+						round(clustersNumber/length(colorPalette26))+1)[1:clustersNumber])
+	}
+	
+	if(length(colorPalette) == 1){
+		if(colorPalette == "default"){
+			return(pickDefaultPalette(clustersNumber, colorPalette26))
+		}
+	}
+	
+	if(clustersNumber > length(colorPalette)){
+		message("The number of clusters is greater than the number of colors.
+						Using default CONCLUS palette instead.")
+		return(pickDefaultPalette(clustersNumber, colorPalette26))
+	}
+	
+	return(colorPalette[1:clustersNumber])
+}
