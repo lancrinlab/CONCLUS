9760d172746a62211a9397831daa2deb8e83d790
Nicolas Descostes <nicolas.descostes@gmail.com>
2019-07-04 11:49:04 +0200

    hide mkOutlierScoreDf and excludeOutliers

--- R/clustering.R
+++ R/clustering.R
@@ -88,3 +88,139 @@ testClustering <- function(sceObject, dataDirectory, experimentName,
 							minNeighbours = minPts)))
 	
 }
+
+
+
+.mkOutlierScoreDf <- function(mat){
+	
+	outlierScoreDf <- as.data.frame(colnames(mat))
+	colnames(outlierScoreDf) <- "cellName"
+	outlierScoreDf <- dplyr::mutate(outlierScoreDf, outlierScore=NA)
+	
+	for(i in 1:ncol(mat)){
+		vec <- mat[,i]
+		outlierScoreDf$outlierScore[i] <- length(vec[vec == 0])
+	}
+	
+	outlierScoreDf$outlierScorePer <- outlierScoreDf$outlierScore / nrow(mat)
+	return(outlierScoreDf)
+}
+
+
+.excludeOutliers <- function(dbscanMatrix, sceObject, threshold=0.3){
+	# exclude outliers based on DBSCAN clustering
+	# outliers are the cells which cannot be assigned
+	# to any final cluster
+	
+	outlierInfo <- .mkOutlierScoreDf(dbscanMatrix)
+	colData <- SummarizedExperiment::colData(sceObject)[SummarizedExperiment::colData(sceObject)$cellName
+					%in% colnames(dbscanMatrix),]
+	
+	if(is.vector(colData)){
+		colData <- S4Vectors::DataFrame(cellName=colData, row.names=colData)
+	}
+	
+	# Case if coldata has the outliers scores already
+	colData$outlierScorePer <- NULL
+	colData$outlierScore <- NULL
+	
+	colData <- merge(colData, outlierInfo,
+			by.x="cellName", by.y="cellName",
+			all.x=TRUE, all.y=TRUE, sort=FALSE)
+	rownames(colData) <- colData$cellName
+	
+	numberOfCellsBefore <- dim(colData)[1]
+	sceObject <- sceObject[,colData$outlierScorePer < threshold]
+	colData <- colData[colData$outlierScorePer < threshold,]
+	numberOfCellsAfter <- dim(colData)[1]
+	
+	dbscanMatrix <- dbscanMatrix[,outlierInfo$outlierScorePer < threshold]
+	SummarizedExperiment::colData(sceObject) <- colData
+	
+	message(paste(numberOfCellsBefore - numberOfCellsAfter,
+					"outliers were excluded from the SingleCellExperiment object.\n"))
+	
+	return(list(sceObject, dbscanMatrix))
+}
+
+
+
+#' DBSCAN clustering on t-SNE results.
+#' 
+#' This function provides consensus DBSCAN clustering based on the results of t-SNE. 
+#' You can tune algorithm parameters in options to get the number of clusters you want.
+#'
+#' @param tSNEResults the result of conclus::generateTSNECoordinates() function.
+#' @param sceObject a SingleCellExperiment object with your experiment.
+#' @param dataDirectory output directory of a given CONCLUS run (supposed to be the same for one experiment during the workflow).
+#' @param experimentName name of the experiment which appears in filenames (supposed to be the same for one experiment during the workflow).
+#' @param epsilon a parameter of fpc::dbscan() function.
+#' @param minPoints a parameter of fpc::dbscan() function.
+#' @param k preferred number of clusters. Alternative to deepSplit.
+#' @param PCs a vector of first principal components.
+#' For example, to take ranges 1:5 and 1:10 write c(5, 10).
+#' @param perplexities a vector of perplexity for t-SNE.
+#' @param randomSeed random seed for reproducibility.
+#' @param deepSplit intuitive level of clustering depth. Options are 1, 2, 3, 4.
+#' @param clusteringMethod a clustering methods passed to hclust() function.
+#' @param cores maximum number of jobs that CONCLUS can run in parallel.
+#' @param deleteOutliers Whether cells which were often defined as outliers by dbscan must be deleted.
+#' It will require recalculating of the similarity matrix of cells. Default is FALSE.
+#' Usually those cells appear in an "outlier" cluster and can be easier distinguished and deleted later
+#' if necessary.
+#' 
+#'
+#' @return A list containing filtered from outliers SingleCellExperiment object and cells similarity matrix.
+#' @export
+runClustering <- function(tSNEResults, # for deleteOutliers = FALSE
+		sceObject, dataDirectory,
+		experimentName, epsilon=c(1.3, 1.4, 1.5),
+		minPoints=c(3, 4), k=0, deepSplit=4,
+		clusteringMethod = "ward.D2",
+		cores=14,
+		deleteOutliers = TRUE,
+		PCs=c(4, 6, 8, 10, 20, 40, 50),
+		perplexities=c(30,40), # for deleteOutliers = TRUE
+		randomSeed = 42){
+	# It combines all the clustering parts. Takes tSNE coordinates and gives
+	# results of final clustering: sceObject and cell correlation matrix
+	
+	# run dbscan
+	message("Running dbscan using ", cores, " cores.")
+	dbscanResults <- runDBSCAN(tSNEResults, sceObject, dataDirectory,
+			experimentName, epsilon=epsilon,
+			minPoints=minPoints, cores=cores)
+	if(deleteOutliers){
+		# filter cluster outliers
+		message("Excluding clustering outliers.")
+		filteredResults <- .excludeOutliers(dbscanResults, sceObject)
+		sceObjectFiltered <- filteredResults[[1]]
+		#dbscanResultsFiltered <- filteredResults[[2]]
+		message("Getting TSNE coordinates for the filtered sceObject.")
+		tSNEResultsFiltered <- generateTSNECoordinates(sceObjectFiltered,
+				dataDirectory,
+				experimentName, PCs=PCs,
+				perplexities=perplexities,
+				randomSeed=randomSeed,
+				cores = cores)
+		
+		dbscanResultsFiltered <- runDBSCAN(tSNEResultsFiltered, sceObjectFiltered,
+				dataDirectory,
+				experimentName, epsilon=epsilon,
+				minPoints=minPoints, cores=cores)
+	}else{
+		sceObjectFiltered = sceObject
+		dbscanResultsFiltered = dbscanResults
+	}
+	
+	# assign cells to cluster
+	clusteringResults <- clusterCellsInternal(dbscanResultsFiltered, sceObjectFiltered,
+			clusterNumber=k, deepSplit=deepSplit,
+			clusteringMethod=clusteringMethod,
+			cores=cores)
+	sceObjectFiltered <- clusteringResults[[1]]
+	cellsSimilarityMatrix <- clusteringResults[[2]]
+	
+	return(list(sceObjectFiltered, cellsSimilarityMatrix))
+}
+
