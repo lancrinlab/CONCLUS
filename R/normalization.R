39358cc83ac50c938702b6e106ba5b0fcaafceea
Nicolas Descostes <nicolas.descostes@gmail.com>
2019-07-04 14:24:10 +0200

    merge the three normalization function by setting databaseDir = TRUE and modifying annotateGene accordingly

--- R/normalization.R
+++ R/normalization.R
@@ -1,5 +1,27 @@
-# Do not export this function.
-.normaliseCountMatrix <- function(countMatrix,
+#' normaliseCountMatrix
+#'
+#' Create a SingleCellExperiment object and perform normalization. The same as conclus::normalizeCountMatrix.
+#'
+#' @param countMatrix a matrix with non-normalised gene expression.
+#' @param species either 'mmu' or 'human'.
+#' @param method a method of clustering: available option is "default" using scran and scater.
+#' @param sizes a vector of size factors from scran::computeSumFactors() function.
+#' @param rowData a data frame with information about genes
+#' @param colData a data frame with information about cells
+#' @param alreadyCellFiltered if TRUE, cells quality check and filtering will not be applied. 
+#' However, the function may delete some cells if they have negative size factors after scran::computeSumFactors.
+#' @param runQuickCluster if scran::quickCluster() function must be applied.
+#' Usually, it allows to improve normalization for medium-size count matrices. 
+#' However, it is not recommended for datasets with less than 200 cells and
+#' may take too long for datasets with more than 10000 cells.
+#' @param databaseDir a path to annotation database provided with CONCLUS called 
+#' "Mmus_gene_database_secretedMol.tsv" (only for MusMusculus 'mmu').
+#' The function will work also without the database but slower because it will retrieve genes info from biomaRt.
+#'
+#' @return A SingleCellExperiment object with normalized gene expression, colData, and rowData.
+#' @export
+
+normaliseCountMatrix <- function(countMatrix,
 		species,
 		method="default",
 		sizes=c(20,40,60,80,100),
@@ -69,73 +91,4 @@
 		message("Wrong method. Unmodified count matrix returned.")
 		return(countMatrix)
 	}
-}
-
-#' normaliseCountMatrix
-#'
-#' Create a SingleCellExperiment object and perform normalization. The same as conclus::normalizeCountMatrix.
-#'
-#' @param countMatrix a matrix with non-normalised gene expression.
-#' @param species either 'mmu' or 'human'.
-#' @param method a method of clustering: available option is "default" using scran and scater.
-#' @param sizes a vector of size factors from scran::computeSumFactors() function.
-#' @param rowData a data frame with information about genes
-#' @param colData a data frame with information about cells
-#' @param alreadyCellFiltered if TRUE, cells quality check and filtering will not be applied. 
-#' However, the function may delete some cells if they have negative size factors after scran::computeSumFactors.
-#' @param runQuickCluster if scran::quickCluster() function must be applied.
-#' Usually, it allows to improve normalization for medium-size count matrices. 
-#' However, it is not recommended for datasets with less than 200 cells and
-#' may take too long for datasets with more than 10000 cells.
-#' @param databaseDir a path to annotation database provided with CONCLUS called 
-#' "Mmus_gene_database_secretedMol.tsv" (only for MusMusculus 'mmu').
-#' The function will work also without the database but slower because it will retrieve genes info from biomaRt.
-#'
-#' @return A SingleCellExperiment object with normalized gene expression, colData, and rowData.
-#' @export
-
-normaliseCountMatrix <- function(countMatrix,
-		species,
-		method="default",
-		sizes=c(20,40,60,80,100),
-		rowData=NULL,
-		colData=NULL,
-		alreadyCellFiltered = FALSE,
-		runQuickCluster = TRUE,
-		databaseDir = TRUE # FALSE for not using the database but download from biomaRt
-){
-	
-	.normaliseCountMatrix(countMatrix,
-			species,
-			method=method,
-			sizes=sizes,
-			rowData=rowData,
-			colData=colData,
-			alreadyCellFiltered = alreadyCellFiltered,
-			runQuickCluster = runQuickCluster,
-			databaseDir = databaseDir)
-}
-# deleted from the description of the normaliseCountMatrix() function:
-# #' @param method a method of clustering: "default" (using scran and scater) or "census" (using Census from Monocle).
-
-#' @export
-normalizeCountMatrix <- function(countMatrix,
-		species,
-		method="default",
-		sizes=c(20,40,60,80,100),
-		rowData=NULL,
-		colData=NULL,
-		alreadyCellFiltered = FALSE,
-		runQuickCluster = TRUE,
-		databaseDir = ""){
-	
-	.normaliseCountMatrix(countMatrix,
-			species,
-			method="default",
-			sizes=c(20,40,60,80,100),
-			rowData=NULL,
-			colData=NULL,
-			alreadyCellFiltered = FALSE,
-			runQuickCluster = TRUE,
-			databaseDir = "")
 }
