9aeb6b6752c27cdc0b8c3d5b9624048642ac2bd4
Nicolas Descostes <nicolas.descostes@embl.it>
2019-07-10 11:07:40 +0200

    Remove doc from the code

--- R/tSNE.R
+++ R/tSNE.R
@@ -1,20 +1,3 @@
-#' Generate and save t-SNE coordinates with selected parameters.
-#'
-#' The function generates several t-SNE coordinates based on given perplexity and ranges of PCs. 
-#' Final number of t-SNE plots is length(PCs)*length(perplexities)
-#' It writes coordinates in "dataDirectory/tsnes" subfolder.
-#'
-#' @param sceObject a SingleCellExperiment object with your experiment.
-#' @param dataDirectory output directory for CONCLUS (supposed to be the same for one experiment during the workflow).
-#' @param experimentName name of the experiment which will appear in filenames (supposed to be the same for one experiment during the workflow).
-#' @param randomSeed random seed for reproducibility.
-#' @param cores maximum number of jobs that CONCLUS can run in parallel.
-#' @param PCs a vector of first principal components.
-#' For example, to take ranges 1:5 and 1:10 write c(5, 10).
-#' @param perplexities a vector of perplexity (t-SNE parameter).
-#'
-#' @return An object with t-SNE results (coordinates for each plot).
-#' @export
 generateTSNECoordinates <- function(sceObject, dataDirectory, experimentName,
 		randomSeed=42, cores=14,
 		PCs=c(4, 6, 8, 10, 20, 40, 50),
