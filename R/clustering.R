dc5467f1d18d161ffc96f61942ae8cdeae252a0b
Nicolas Descostes <nicolas.descostes@embl.it>
2019-07-11 22:02:05 +0200

    remove doc

--- R/clustering.R
+++ R/clustering.R
@@ -21,26 +21,6 @@
 			legend="bottom")
 }
 
-#' To check one iteration of clustering before running full workflow CONCLUS.
-#' 
-#' This function generates a single clustering iteration of CONCLUS to check whether
-#' chosen parameters for dbscan are suitable for your data.
-#'
-#' @param sceObject a SingleCellExperiment object with your experiment.
-#' @param dataDirectory output directory (supposed to be the same for one experiment during the workflow).
-#' @param experimentName name of the experiment which will appear in filenames (supposed to be the same for one experiment during the workflow).
-#' @param dbscanEpsilon a parameter of fpc::dbscan() function.
-#' @param minPts a parameter of fpc::dbscan() function.
-#' @param PCs a vector of PCs for plotting.
-#' @param perplexities vector of perplexities (t-SNE parameter).
-#' @param randomSeed random seed for reproducibility.
-#' @param width plot width.
-#' @param height plot height.
-#' @param ... other pdf() arguments.
-#'
-#' @return t-SNE results, a distance graph plot, a t-SNE plot colored by test clustering solution.
-#' @export
-
 testClustering <- function(sceObject, dataDirectory, experimentName,
 		dbscanEpsilon=1.4,
 		minPts=5,
