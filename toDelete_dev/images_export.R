a5098e08b6a165c441fd364749675793f9e6c84f
Nicolas Descostes <nicolas.descostes@embl.it>
2019-07-18 17:36:58 +0200

    tmp commit
    need to rebuild package

--- toDelete_dev/images_export.R
+++ toDelete_dev/images_export.R
@@ -74,8 +74,6 @@ tSNEResults <- generateTSNECoordinates(sceObject, outputDirectory,
 		randomSeed = randomSeed)
 
 ncol(tSNEResults)
-# the third matrix of t-SNE coordinates with PC = 8 and perplixities = 30
-# it is saved as "tsnes/Bergiers_tsne_coordinates_3_8PCs_30perp.tsv"
 head(tSNEResults[1,3][[1]])
 
 
@@ -133,9 +131,18 @@ if(any(colnames(SummarizedExperiment::colData(sceObjectFiltered))
 			returnPlot = TRUE)
 }
 
-ggsave(filename = paste0(outputDirectory, "fig3_tSNEColor_clusters.svg"), plot = tSNEclusters[[5]], device = svg)
-ggsave(filename = paste0(outputDirectory, "fig4_tSNEnoColor.svg"), plot = tSNEnoColor[[5]], device = svg)
-ggsave(filename = paste0(outputDirectory, "fig5_tSNEState.svg"), plot = tSNEstate[[5]], device = svg)
+png(paste0(outputDirectory, "fig4_tSNEColor_clusters.png"), width = 700, height=700, res=100, type = "cairo-png")
+tSNEclusters[[5]]
+dev.off()
+
+png(paste0(outputDirectory, "fig5_tSNEnoColor.png"), width = 700, height=700, res=100, type = "cairo-png")
+tSNEnoColor[[5]]
+dev.off()
+
+png(paste0(outputDirectory, "fig6_tSNEState.png"), width = 700, height=700, res=100, type = "cairo-png")
+tSNEstate[[5]]
+dev.off()
+
 
 
 ### Cell similarity heatmap
@@ -149,7 +156,7 @@ clustersNumber <- length(unique(
 				SummarizedExperiment::colData(sceObjectFiltered)$clusters))
 colorPalette <- conclus::choosePalette(colorPalette, clustersNumber)
 
-png(paste0(outputDirectory, "fig6_plotCellSimilarity.png"), width = 700, height=700, res = 100)
+png(paste0(outputDirectory, "fig7_plotCellSimilarity.png"), width = 700, height=700, res = 100)
 conclus::plotCellSimilarity(sceObjectFiltered, cellsSimilarityMatrix, outputDirectory,
 		experimentName, colorPalette, 
 		orderClusters = orderClusters, 
@@ -161,7 +168,7 @@ dev.off()
 
 ### Cluster similarity heatmap
 
-png(paste0(outputDirectory, "fig7_plotClustersSimilarity.png"), width = 700, height=700)
+png(paste0(outputDirectory, "fig8_plotClustersSimilarity.png"), width = 700, height=700, res=100)
 conclus::plotClustersSimilarity(clustersSimilarityMatrix, 
 		sceObjectFiltered,
 		dataDirectory = outputDirectory, 
@@ -202,7 +209,7 @@ orderClusters <- T # F will apply hierarchical clustering to all cells
 orderGenes <- T    # F will apply hierarchical clustering to all genes
 meanCentered <- T  # F to show normalized counts
 
-png(paste0(outputDirectory, "fig8_positivemarkers.png"), width = 700, height=700, res = 150)
+png(paste0(outputDirectory, "fig9_positivemarkers.png"), width = 700, height=700, res = 160)
 conclus::plotCellHeatmap(markersClusters, sceObjectFiltered, outputDirectory, 
 		experimentName, 
 		paste0("clusters",
@@ -233,7 +240,7 @@ orderClusters <- T # F will apply hierarchical clustering to all cells
 orderGenes <- T    # F will apply hierarchical clustering to all genes
 meanCentered <- F  # F to show normalized counts
 
-png(paste0(outputDirectory, "fig9_positivemarkers-unnorm.png"), width = 700, height=700, res = 150)
+png(paste0(outputDirectory, "fig10_positivemarkers-unnorm.png"), width = 700, height=700, res = 160)
 conclus::plotCellHeatmap(markersClusters, sceObjectFiltered, outputDirectory, 
 		experimentName, 
 		paste0("clusters",
@@ -260,24 +267,124 @@ dev.off()
 
 # Plot t-SNE colored by expression of a selected gene
 
-
-ccl3 <- plotGeneExpression("Ccl3", experimentName, outputDirectory, sceObjectFiltered, 
+png(paste0(outputDirectory, "fig11_positivemarkers-ccl3.png"), width = 700, height=700, res = 100)
+plotGeneExpression("Ccl3", experimentName, outputDirectory, sceObjectFiltered, 
 		tSNEpicture = 10, returnPlot=T)
+dev.off()
 
-ggsave(filename = paste0(outputDirectory, "fig10_tSNESccl3.svg"), plot = ccl3, device = svg)
 
-
+png(paste0(outputDirectory, "fig12_positivemarkers-Gp9.png"), width = 700, height=700, res = 100)
 plotGeneExpression("Gp9", experimentName, outputDirectory, sceObjectFiltered, 
 		tSNEpicture = 10, returnPlot=T)
+dev.off()
 
+png(paste0(outputDirectory, "fig13_positivemarkers-Fn1.png"), width = 700, height=700, res = 100)
 plotGeneExpression("Fn1", experimentName, outputDirectory, sceObjectFiltered, 
 		tSNEpicture = 10, returnPlot=T)
+dev.off()
 
+png(paste0(outputDirectory, "fig14_positivemarkers-Alox5ap.png"), width = 700, height=700, res = 100)
 plotGeneExpression("Alox5ap", experimentName, outputDirectory, sceObjectFiltered, 
 		tSNEpicture = 10, returnPlot=T)
-		```
+dev.off()
+
+
+# Collect publicly available info about marker genes
+
+## Collect information for the top 10 markers for each cluster
+
+result <- getGenesInfo(markersClusters, groupBy = "clusters",
+		getUniprot = TRUE)
+
+outputDir <- file.path(outputDirectory, "/marker_genes/getGenesInfo")
+dir.create(outputDir, showWarnings=F)
+write.table(result, file = file.path(outputDir, 
+				"Bergiers_markersClusters_top10_clusters9_genesInfo.csv"),
+		quote = FALSE, sep = ";", row.names = FALSE)
+
+
+## Collect information for the top X markers for each cluster
+
+saveMarkersLists(experimentName, outputDirectory)
+saveGenesInfo(outputDirectory, sep = ";", header = TRUE, 
+		startFromFile = 10, getUniprot = TRUE)
+
+
+# Supervised clustering 
+
+exportClusteringResults(sceObjectFiltered, outputDirectory, experimentName, 
+		"clusters_table.tsv")
+clustersTable <- read.delim(file.path(outputDirectory, "output_tables", 
+				paste0(experimentName, "_clusters_table.tsv")), stringsAsFactors = FALSE) 
+
+clustersTable$clusters[clustersTable$clusters == "3"] = "2"
+clustersTable$clusters[clustersTable$clusters == "9"] = "8"
+clustersTable$clusters[clustersTable$clusters == "10"] = "8"
+
+write.table(clustersTable, file.path(outputDirectory, "output_tables", 
+				paste0(experimentName, "_clusters_table_manual.tsv")), 
+		quote = FALSE, sep = "\t")
 
 
 
+sceObjectFiltered <- addClusteringManually(fileName = "clusters_table_manual.tsv", 
+		dataDirectory = outputDirectory, 
+		experimentName = experimentName,
+		sceObject = sceObjectFiltered, 
+		columnName = "clusters")
+
+# Redo the analysis with manual clustering
+sceObjectFiltered2 <- runCONCLUS(outputDirectory, experimentName, 
+		statePalette= c("bisque", "cadetblue2", "coral1", "cornflowerblue"),
+		preClustered = TRUE, manualClusteringObject = sceObjectFiltered, k=7, cores=10)
+
+
+dataDirectory = outputDirectory 
+statePalette= c("bisque", "cadetblue2", "coral1", "cornflowerblue")
+preClustered = TRUE
+manualClusteringObject = sceObjectFiltered
+cores=10
+columnsMetaData = NA
+species = NA
+colorPalette="default"
+clusteringMethod="ward.D2"
+epsilon=c(1.3, 1.4, 1.5)
+minPoints=c(3, 4)
+k=7
+PCs=c(4, 6, 8, 10, 20, 40, 50)
+perplexities=c(30,40)
+randomSeed = 42
+deepSplit=4
+orderClusters = FALSE
+plotPDFcellSim = TRUE
+deleteOutliers = TRUE
+tSNEalreadyGenerated = FALSE
+tSNEresExp = ""
+
+
+
+
+meanCentered <- T  # F to show normalized counts, 
+plotCellHeatmap(markersClusters, sceObjectFiltered, outputDirectory, 
+		experimentName, 
+		paste0("clusters",
+				length(levels(
+								SummarizedExperiment::colData(sceObjectFiltered)$clusters)),
+				"_meanCentered",meanCentered,"_orderClusters",orderClusters,
+				"_orderGenes",orderGenes,"_top",
+				genesNumber, "markersPerCluster"), 
+		meanCentered = meanCentered, 
+		colorPalette = c("#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C",
+				"#FB9A99", "#E31A1C", "#FDBF6F"),
+		orderClusters = orderClusters,
+		orderGenes = orderGenes,
+		fontsize_row = 5,
+		statePalette = c("bisque", "cadetblue2", 
+				"coral1", "cornflowerblue"),
+		color = colorRampPalette(c("#023b84","#4b97fc", 
+						"#FEE395", 
+						"#F4794E", "#D73027",
+						"#a31008","#7a0f09"))(100),
+		returnPlot = TRUE)
 
 
